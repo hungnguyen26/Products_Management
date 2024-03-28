8f2407c9baa89df8b7d11ec99964130b2d1a2d0f
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-03-28 14:39:02 +0700

    tính năng tìm kiếm admin ( FE)

--- public/admin/js/js.js
+++ public/admin/js/js.js
@@ -18,3 +18,21 @@ if(buttonStatus.length > 0){
     })
 }
 // end button status
+
+// form search
+const formSearch = document.querySelector("#form-search");
+if(formSearch){
+    let url = new URL(window.location.href);
+
+    formSearch.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        const keyword = event.target.elements.keyword.value;
+        if(keyword){
+            url.searchParams.set("keyword",keyword);
+        }else{
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href;
+    })
+}
+//end  form search
