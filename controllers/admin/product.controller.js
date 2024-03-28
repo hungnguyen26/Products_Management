d6d09b1e3b399b2543e67b55a54d60041dc315a2
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-03-28 23:22:12 +0700

    tính năng tìm kiếm admin backend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -35,18 +35,29 @@ module.exports.product = async (req, res) => {
     }
     
     let find = {
-         deleted: false
+         deleted: false,
     }
 
     if(req.query.status){
         find.status = req.query.status;  // truyển thêm 1 key status vào obj find
     }
+
+    let keyword = "";
+    if(req.query.keyword){
+        keyword = req.query.keyword;
+
+        const regex = new RegExp(keyword,"i");   // i không phân biệt chữ hoa thường
+
+        find.title = regex; 
+    }
+
     const product = await Product.find(find);
 
     // console.log(product);
     res.render("admin/pages/product/index.pug",{
         pageTitle: "Trang danh sách sản phẩm",
         products:product,
-        filterStatus:filterStatus
-    });
+        filterStatus:filterStatus,
+        keyword:keyword
+    }); 
 }
