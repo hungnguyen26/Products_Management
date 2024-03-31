41f6732a08a5aaefdfcd69f8be61c0af90804e19
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-03-31 16:25:24 +0700

    tính năng phân trang admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,37 +1,59 @@
-// [GET] admin/product
-const Product = require("../../models/products-model");
-const filterStatusHelper = require("../../helpers/filterStatus");
-const searchHelper = require("../../helpers/search");
+    // [GET] admin/product
+    const Product = require("../../models/products-model");
+    const filterStatusHelper = require("../../helpers/filterStatus");
+    const searchHelper = require("../../helpers/search");
 
 
-module.exports.product = async (req, res) => {
+    module.exports.product = async (req, res) => {
+        
+        //đoạn bộ lọc
+    const filterStatus =  filterStatusHelper(req.query);
+    //    console.log(filterStatus);
+        
+        let find = {
+            deleted: false,
+        }
+
+        if(req.query.status){
+            find.status = req.query.status;  // truyển thêm 1 key status vào obj find
+        }
+        //end đoạn bộ lọc
+
+
+        // phần tìm kiếm
+        const objectSearch = searchHelper(req.query);
+        if(objectSearch.regex ){
+            find.title = objectSearch.regex;
+        }
+        // end phần tìm kiếm
+
+
+
+        // phần phân trang 
+        let objectPagination = {
+            limitItem: 4,   // sl phần tử mỗi trang
+            currentPage:1
+        };
     
-    //đoạn bộ lọc
-   const filterStatus =  filterStatusHelper(req.query);
-   console.log(filterStatus);
-    
-    let find = {
-         deleted: false,
+        if(req.query.page){
+            objectPagination.currentPage = parseInt(req.query.page);
+        }
+
+        objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
+
+        const countProduct = await Product.countDocuments(find);  // đếm tổng số sản phẩm
+        const totalPage =Math.ceil(countProduct/objectPagination.limitItem);
+        objectPagination.totalPage = totalPage;
+        // end phần phân trang 
+
+
+        const product = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);   //limit là số lượng phần tử hiển thị
+
+        res.render("admin/pages/product/index.pug",{
+            pageTitle: "Trang danh sách sản phẩm",
+            products:product,
+            filterStatus:filterStatus,
+            keyword:objectSearch.keyword,
+            pagination:objectPagination
+        }); 
     }
-
-    if(req.query.status){
-        find.status = req.query.status;  // truyển thêm 1 key status vào obj find
-    }
-
-    // phần tìm kiếm
-    const objectSearch = searchHelper(req.query);
-    if(objectSearch.regex ){
-        find.title = objectSearch.regex;
-    }
-
-
-    const product = await Product.find(find);
-
-    // console.log(product);
-    res.render("admin/pages/product/index.pug",{
-        pageTitle: "Trang danh sách sản phẩm",
-        products:product,
-        filterStatus:filterStatus,
-        keyword:objectSearch.keyword
-    }); 
-}
--- public/admin/js/js.js
+++ public/admin/js/js.js
@@ -36,3 +36,21 @@ if(formSearch){
     })
 }
 //end  form search
+
+//phân trang pegination
+const buttonPagination = document.querySelectorAll("[button-pagination]");
+if(buttonPagination){
+    let url = new URL(window.location.href);
+            
+    buttonPagination.forEach((button)=>{
+        button.addEventListener("click", ()=>{
+            const page = button.getAttribute("button-pagination");
+
+            url.searchParams.set("page",page);
+
+            window.location.href = url.href;
+
+        })
+    })
+}
+//end phân trang
