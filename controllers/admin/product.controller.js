dff3038011a82f8de89a008d3a9a32600f058290
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-04-14 23:40:04 +0700

    tính năng chỉnh sửa sản phẩm admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -140,7 +140,6 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/product/create
 module.exports.createPost = async (req, res) => {
-  
   req.body.price = parseInt(req.body.price);
   req.body.discountPercentage = parseInt(req.body.discountPercentage);
   req.body.stock = parseInt(req.body.stock);
@@ -160,4 +159,45 @@ module.exports.createPost = async (req, res) => {
   await product.save(); // lưu vào database
 
   res.redirect(`${systemConfig.prefixAdmin}/products`);
+};
+
+// [GET] /admin/product/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      _id: req.params.id,
+    };
+
+    const product = await Product.findOne(find);
+
+    res.render("admin/pages/product/edit.pug", {
+      pageTitle: "Chỉnh sửa sản phẩm",
+      product: product,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products`);
+  }
+};
+
+// [PATCH] /admin/product/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  req.body.price = parseInt(req.body.price);
+  req.body.discountPercentage = parseInt(req.body.discountPercentage);
+  req.body.stock = parseInt(req.body.stock);
+  req.body.position = parseInt(req.body.position);
+
+  if (req.file) {
+    req.body.thumbnail = `/uploads/${req.file.filename}`;
+  }
+
+  try {
+    await Product.updateOne({ _id: id }, req.body);
+    req.flash("success", `Cập nhật thành công sản phẩm!`);
+  } catch (error) {
+    req.flash("error", `Cập nhật thất bại!`);
+  }
+
+  res.redirect(`back`);
 };
--- routes/admin/product.router.js
+++ routes/admin/product.router.js
@@ -18,9 +18,20 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
 
-router.post("/create", 
-    upload.single("thumbnail"),
-    validate.createPost,
-    controller.createPost)
+router.post(
+  "/create",
+  upload.single("thumbnail"),
+  validate.createPost,
+  controller.createPost
+);
+
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+  "/edit/:id",
+  upload.single("thumbnail"),
+  validate.createPost,
+  controller.editPatch
+);
 
 module.exports = router;
