1e9cdabc1e15a983b3dbe44ce50cedb15403b2cf
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-04-30 22:57:27 +0700

    fix lỗi số thứ tự Danh mục và Danh sách sản phẩm

--- helpers/create-tree.js
+++ helpers/create-tree.js
@@ -1,19 +1,24 @@
-const createTree = (arr, parentId = '')=>{
-    const tree = [];
-    arr.forEach(item => {
-      if(item.parent_id === parentId){
-        const newItem  = item;
-        const children = createTree(arr, item.id);
-        if(children.length > 0){
-          newItem.children = children;
-        }
-        tree.push(newItem);
-      }
-    });
-    return tree;
-}
+var count = 0;
+const createTree = (arr, parentId = "") => {
+  const tree = [];
 
-module.exports.tree = (arr, parentId = '')=>{
-    const tree = createTree(arr, parentId = '');
-    return tree;
-}
+  arr.forEach((item) => {
+    if (item.parent_id === parentId) {
+      count++;
+      const newItem = item;
+      newItem.index = count;
+      const children = createTree(arr, item.id);
+      if (children.length > 0) {
+        newItem.children = children;
+      }
+      tree.push(newItem);
+    }
+  });
+  return tree;
+};
+
+module.exports.tree = (arr, parentId = "") => {
+  count = 0;
+  const tree = createTree(arr, (parentId = ""));
+  return tree;
+};
