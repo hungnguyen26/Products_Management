8e24e59adce95e5c2cf1c823649656ade5032eaf
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-05-09 00:44:26 +0700

    Tính năng đăng xuất

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -41,3 +41,9 @@ module.exports.loginPost = async (req, res) => {
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
 
 };
+
+// [GET] admin/auth/logout
+module.exports.logout = async (req, res) => {
+    res.clearCookie("token");
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+  };
--- routes/admin/auth.router.js
+++ routes/admin/auth.router.js
@@ -8,4 +8,6 @@ router.get("/login", controller.login);
 
 router.post("/login", validate.loginPost, controller.loginPost);
 
+router.get("/logout",  controller.logout);
+
 module.exports = router;
