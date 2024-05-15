9a8454133d081020967f9bbaf639bc2d9d278bb7
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-05-16 00:24:08 +0700

    Hiển thị danh sách sản phẩm mới nhất bên client

--- controllers/client/home_controller.js
+++ controllers/client/home_controller.js
@@ -7,11 +7,23 @@ module.exports.home = async (req, res) => {
     featured: "1",
     deleted:false,
     status:"active"
-  })
+  }).limit(6);
+
+  const newProductsFeatured = ProductsHelper.priceNewProducts(productsFeatured);
   // end lấy ra sản phẩm nổi bật
-  const newProducts = ProductsHelper.priceNewProducts(productsFeatured);
+
+  // hiển thị danh sách sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted:false,
+    status: "active"
+  }).sort({position: "desc"}).limit(6); 
+  const newProductsnew = ProductsHelper.priceNewProducts(productsNew);
+
+  // end hiển thị danh sách sản phẩm mới nhất
+
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsnew
   });
 };
