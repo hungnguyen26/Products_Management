37d31d88f643ab163d45ba26070018efe067587f
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-05-16 17:57:54 +0700

    Trang danh sách sản phẩm bên client

--- controllers/client/product_controller.js
+++ controllers/client/product_controller.js
@@ -1,39 +1,81 @@
-const Product = require("../../models/products-model")
+const Product = require("../../models/products-model");
 const ProductsHelper = require("../../helpers/product");
+
+const ProductsCategory = require("../../models/products-category.model");
 
 // [GET] product
 module.exports.product = async (req, res) => {
-    const products = await Product.find({
-        status:"active",
-        deleted: false
-    }).sort({position:"desc"});
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" });
 
-    const newProducts = ProductsHelper.priceNewProducts(products);
+  const newProducts = ProductsHelper.priceNewProducts(products);
 
-    res.render("client/pages/product/index.pug",{
-        pageTitle:"Trang danh sách sản phẩm",
-        products: newProducts
-    })
-}
+  res.render("client/pages/product/index.pug", {
+    pageTitle: "Trang danh sách sản phẩm",
+    products: newProducts,
+  });
+};
 
 // [GET] /product/:slug
 module.exports.detail = async (req, res) => {
-    try {
-        const find = {
-          deleted: false,
-          slug: req.params.slug,
-          status:"active"
-        };
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slug,
+      status: "active",
+    };
+
+    const product = await Product.findOne(find);
+
+    // console.log(product);
+
+    res.render("client/pages/product/detail.pug", {
+      pageTitle: product.title,
+      product: product,
+    });
+  } catch (error) {
+    res.redirect(`back`);
+  }
+};
+
+// [GET] /product/:slug
+module.exports.Category = async (req, res) => {
+  const category = await ProductsCategory.findOne({
+    slug: req.params.slugCategory,
+    status: "active",
+    deleted: false,
+  });
+
+  const getSubCategory = async (parentID) => {
+    const subs = await ProductsCategory.find({
+      parent_id: parentID,
+      status: "active",
+      deleted: false,
+    });
+    let allsubs = [...subs];
     
-        const product = await Product.findOne(find);
+    for (const sub of subs) {
+      const childs = await getSubCategory(sub.id);
+      allsubs = allsubs.concat(childs)
+    }
+    return allsubs;
+  };
 
-        // console.log(product);
+  const listSubCategory = await getSubCategory(category.id);
+  const listSubCategoryId = listSubCategory.map(item=> item.id)
+  console.log(listSubCategoryId);
 
-        res.render("client/pages/product/detail.pug", {
-          pageTitle: product.title,
-          product: product
-        });
-      } catch (error) {
-        res.redirect(`back`);
-      }
-}
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false,
+  }).sort({ position: "desc" });
+
+  const newProducts = ProductsHelper.priceNewProducts(products);
+
+  res.render("client/pages/product/index.pug", {
+    pageTitle: category.title,
+    products: newProducts,
+  });
+};
--- routes/client/product_router.js
+++ routes/client/product_router.js
@@ -5,6 +5,8 @@ const controller = require("../../controllers/client/product_controller")
 
 router.get('/', controller.product);
 
-router.get('/:slug', controller.detail);
+router.get('/:slugCategory', controller.Category);
+
+// router.get('/:slug', controller.detail);
 
 module.exports = router;
