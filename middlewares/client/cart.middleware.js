ff556b0e57f1a26ca9a213da4245a3318e8f7a28
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-05-19 20:34:52 +0700

    Làm nút mini cart trên header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -9,6 +9,15 @@ module.exports.cartID = async (req,res, next)=>{
         res.cookie("cartID",cart.id,{ expires: new Date(Date.now() + exTime)});  // cookie hết hạn sau 1 năm nữa
     }else{
         // khi đã có giỏ hàng
+        const cart = await Cart.findOne({    // lấy ra id giỏ hàng
+            _id: req.cookies.cartID
+        })
+
+        cart.totalQuantity = cart.products.reduce((sum, item)=>{     // thêm totalQuantity cho cart
+            return sum + item.quantity;
+        },0)                // 0 là giá trị khởi tạo
+
+        res.locals.miniCart = cart;    // tạo biến toàn cục giỏ hàng
     }
     next();
 }
