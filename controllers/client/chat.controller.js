eb45a04f92cf7be4915b9b8369548a5ae3b54858
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-06-11 15:30:51 +0700

    Tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,16 +1,39 @@
-const { Socket } = require("socket.io");
+const Chat = require("../../models/chats.model");
+const User = require("../../models/users.model");
+
 
 // [GET] /chat
 module.exports.index = async (req, res) => {
+    const user_id = res.locals.user.id;
     // SocketIO
-    _io.on('connection', (socket) => {
-      console.log('a user connected',socket.id);
-    
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESS", async (content)=>{
+          // khi có data thì lưu vào dbnguyenhuuhung2610@gmail.co
+          const chat = new Chat({
+            user_id: user_id,
+            content: content
+          });
+          await chat.save();
+        });
     });
     // END SocketIO
+
+    // lấy ra data chat
+    const chats = await Chat.find({
+      deleted:false
+    });
+
+    for (const chat of chats) {
+      const inforUser = await User.findOne({
+        _id: chat.user_id
+      }).select("fullName");
+      
+      chat.inforUser = inforUser;
+    }
 
 
     res.render("client/pages/chat/index.pug", {
         pageTitle: "Chat",
+        chats: chats
       });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,14 @@
-var socket = io();
+// CLIENT SEND MESS
+const formSendData = document.querySelector(".chat .inner-form");
+if(formSendData){
+    formSendData.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const content = e.target.elements.content.value;
+
+        if(content){
+            socket.emit("CLIENT_SEND_MESS",content);
+            e.target.elements.content.value = "";
+        }
+    })
+}
+// end  CLIENT SEND MESS
